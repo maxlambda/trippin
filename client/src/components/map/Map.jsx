5c14d42ecebdeaa86ee76afa0be9748a24ecf335
Max Lam <maxlam0207@gmail.com>
2023-01-15 21:13:10 -0500

    Add fork me on github badge

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -287,6 +287,17 @@ function Map() {
             }}
           />
         </div>
+        <a className="github" href="https://github.com/maxlambda/trippin">
+          <img
+            decoding="async"
+            loading="lazy"
+            width="149"
+            height="149"
+            src="https://github.blog/wp-content/uploads/2008/12/forkme_right_red_aa0000.png?resize=149%2C149"
+            class="attachment-full size-full"
+            alt="Fork me on GitHub"
+            data-recalc-dims="1"/>
+        </a>
 
         {currentUser && addingPin &&
           <div className="geocoder">
